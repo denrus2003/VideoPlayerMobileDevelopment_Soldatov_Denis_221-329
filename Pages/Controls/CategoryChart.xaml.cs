3ac92c2942ae7aaa120cdc55941a0c5dc79ca68f
denis <denis@P9AB>
2024-12-20 10:32:38 +0300

    Добавьте файлы проекта.

---
+++ Pages/Controls/CategoryChart.xaml.cs
@@ -0,0 +1,10 @@
+namespace VideoPlayer.Pages.Controls
+{
+    public partial class CategoryChart
+    {
+        public CategoryChart()
+        {
+            InitializeComponent();
+        }
+    }
+}
