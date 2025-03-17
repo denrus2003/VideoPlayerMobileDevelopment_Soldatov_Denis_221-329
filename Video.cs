b4120a7205c502306228b823dbcef7e8e4225523
Денис Солдатов <denverrus2003@gmail.com>
2025-03-17 11:25:15 +0300

    Добавьте файлы проекта.

---
+++ Video.cs
@@ -0,0 +1,17 @@
+﻿namespace VideoPlayerLibrary
+{
+    /// <summary>
+    /// Класс, представляющий видео.
+    /// </summary>
+    public class Video
+    {
+        public string Title { get; set; }
+        public string FilePath { get; set; }
+
+        public Video(string title, string filePath)
+        {
+            Title = title;
+            FilePath = filePath;
+        }
+    }
+}
